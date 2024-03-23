88a6fc08e8235bdd770f3e6b4a2346e8fd7d6872
Luca Di Gianni <luca.dgni@gmail.com>
2024-03-23 01:05:17 +0100

    style: update primary background color

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       backgroundColor: {
-        primary: "#09090b",
+        primary: "#111110",
       },
       colors: {
         primary: "#fafafa",
