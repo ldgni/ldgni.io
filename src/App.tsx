c2b1c05f9a5512f8c3e406daf87d482763ff1fde
Luca Di Gianni <luca.dgni@gmail.com>
2024-03-27 12:20:13 +0100

    style: extend container size

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ export default function App() {
   };
 
   return (
-    <div className="container max-w-md p-8">
+    <div className="container max-w-lg p-8">
       <motion.header
         className="mb-8 flex items-center justify-between"
         initial="hidden"
