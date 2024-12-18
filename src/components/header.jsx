f736e254f22680c4e92e57c82dc154283b6205d9
Luca Di Gianni <luca.dgni@gmail.com>
2024-12-18 11:36:11 +0100

    feat: add icon linked to writings in header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,4 @@
-import { Github, Mail } from "lucide-react";
+import { BookText, Github, Mail } from "lucide-react";
 
 export default function Header() {
   return (
@@ -20,6 +20,13 @@ export default function Header() {
           aria-label="Send an email to Luca Di Gianni"
           className="rounded-xl p-2 text-neutral-500 transition-colors hover:bg-neutral-300 hover:text-dark dark:hover:bg-neutral-800 dark:hover:text-light">
           <Mail />
+        </a>
+        <a
+          href="https://writings.ldgni.io"
+          target="_blank"
+          aria-label="Luca Di Gianni's writings"
+          className="rounded-xl p-2 text-neutral-500 transition-colors hover:bg-neutral-300 hover:text-dark dark:hover:bg-neutral-800 dark:hover:text-light">
+          <BookText />
         </a>
       </div>
     </header>
