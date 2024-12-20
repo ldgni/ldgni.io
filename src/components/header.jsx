1cde72cbd42ecbc3d2ff5a64fb6b30b50bcdb613
Luca Di Gianni <luca.dgni@gmail.com>
2024-12-20 12:01:43 +0100

    style: adjust padding for header icons

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -12,20 +12,20 @@ export default function Header() {
           href="https://github.com/ldgni"
           target="_blank"
           aria-label="Luca Di Gianni's GitHub profile"
-          className="rounded-xl p-2 text-neutral-500 transition-colors hover:bg-neutral-300 hover:text-dark dark:hover:bg-neutral-800 dark:hover:text-light">
+          className="rounded-xl p-1 text-neutral-500 transition-colors hover:bg-neutral-300 hover:text-dark sm:p-2 dark:hover:bg-neutral-800 dark:hover:text-light">
           <Github />
         </a>
         <a
           href="mailto:luca.dgni@gmail.com"
           aria-label="Send an email to Luca Di Gianni"
-          className="rounded-xl p-2 text-neutral-500 transition-colors hover:bg-neutral-300 hover:text-dark dark:hover:bg-neutral-800 dark:hover:text-light">
+          className="rounded-xl p-1 text-neutral-500 transition-colors hover:bg-neutral-300 hover:text-dark sm:p-2 dark:hover:bg-neutral-800 dark:hover:text-light">
           <Mail />
         </a>
         <a
           href="https://writings.ldgni.io"
           target="_blank"
           aria-label="Luca Di Gianni's writings"
-          className="rounded-xl p-2 text-neutral-500 transition-colors hover:bg-neutral-300 hover:text-dark dark:hover:bg-neutral-800 dark:hover:text-light">
+          className="rounded-xl p-1 text-neutral-500 transition-colors hover:bg-neutral-300 hover:text-dark sm:p-2 dark:hover:bg-neutral-800 dark:hover:text-light">
           <BookText />
         </a>
       </div>
