3be971c6141cb0b44c8c743c29e8ab74d1d3752f
Luca Di Gianni <luca.dgni@gmail.com>
2025-01-01 00:41:53 +0100

    feat: enhance not-found page

--- src/routes/not-found.tsx
+++ src/routes/not-found.tsx
@@ -1,23 +1,21 @@
 import { Link } from "react-router";
 
+import Container from "@/components/container";
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center gap-2">
-      <h1 className="sr-only">Starter Template Vite</h1>
-      <span className="text-6xl font-bold text-red-500 opacity-75 transition-opacity duration-300 hover:opacity-100 sm:text-7xl md:text-8xl">
-        404
-      </span>
-      <span className="text-6xl font-bold text-blue-500 opacity-75 transition-opacity duration-300 hover:opacity-100 sm:text-7xl md:text-8xl">
-        Not
-      </span>
-      <span className="text-6xl font-bold text-orange-500 opacity-75 transition-opacity duration-300 hover:opacity-100 sm:text-7xl md:text-8xl">
-        Found
-      </span>
+    <Container>
+      <Header />
+      <h2>This page doesn't exist.</h2>
+      <span>&rarr; &nbsp;</span>
       <Link
         to="/"
-        className="mt-4 rounded-full border-2 border-neutral-600 px-4 py-2 text-2xl text-neutral-600 transition duration-300 hover:border-neutral-600 hover:bg-neutral-600 hover:text-slate-200 sm:mt-6 sm:text-3xl md:mt-8 md:text-4xl">
-        Home
+        className="duraiton-300 underline underline-offset-2 transition-opacity hover:opacity-75">
+        Go back home
       </Link>
-    </div>
+      <Footer />
+    </Container>
   );
 }
