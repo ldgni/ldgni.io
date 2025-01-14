888ae20b30056f9586f591627242e38d1ac37312
Luca Di Gianni <luca.dgni@gmail.com>
2025-01-14 10:30:03 +0100

    fix: enhance personal description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,12 @@ export default function Page() {
       <div className="mb-8 flex flex-col gap-4">
         <p>
           I&apos;m an IT technician and web developer focused on creating
-          effective digital solutions. I aim to deliver efficient and
-          user-friendly solutions that optimize user experience.
+          effective digital solutions.
+        </p>
+        <p>
+          I have a keen interest in exploring emerging technologies,
+          understanding system architectures, and finding innovative ways to
+          optimize workflows.
         </p>
         <p>
           I prioritize simplicity and functionality, constantly seeking to
