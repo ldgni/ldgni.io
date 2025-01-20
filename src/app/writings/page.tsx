6acac96704325e0376347ab6f22c4de55ee95c6a
Luca Di Gianni <luca.dgni@gmail.com>
2025-01-20 17:33:22 +0100

    feat: add metadata for Projects and Writings pages

--- src/app/writings/page.tsx
+++ src/app/writings/page.tsx
@@ -1,5 +1,9 @@
 import WritingsList from "@/components/writings-list";
 
+export const metadata = {
+  title: "Writings",
+};
+
 export default function WritingsPage() {
   return <WritingsList />;
 }
