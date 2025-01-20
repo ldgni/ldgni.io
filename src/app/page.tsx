6f6932207e58bfdd06cd365795c87ca36b574562
Luca Di Gianni <luca.dgni@gmail.com>
2025-01-20 17:33:59 +0100

    fix: remove unnecessary fragment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,19 @@
 export default function Page() {
   return (
-    <>
-      <div className="mb-8 flex flex-col gap-6">
-        <p>
-          I&apos;m an IT technician and developer focused on creating effective
-          and optimal solutions.
-        </p>
-        <p>
-          My interests revolve around emerging technologies, web development,
-          system optimization and cybersecurity.
-        </p>
-        <p>
-          I prioritize simplicity and functionality, constantly seeking to
-          expand my knowledge and tackle challenges, be it through programming
-          or technical problem-solving.
-        </p>
-      </div>
-    </>
+    <div className="mb-8 flex flex-col gap-6">
+      <p>
+        I&apos;m an IT technician and developer focused on creating effective
+        and optimal solutions.
+      </p>
+      <p>
+        My interests revolve around emerging technologies, web development,
+        system optimization and cybersecurity.
+      </p>
+      <p>
+        I prioritize simplicity and functionality, constantly seeking to expand
+        my knowledge and tackle challenges, be it through programming or
+        technical problem-solving.
+      </p>
+    </div>
   );
 }
