c1ad98f9f00bedaef907696371073bdce8750f67
Luca Di Gianni <luca.dgni@gmail.com>
2025-01-21 11:24:41 +0100

    fix: remove metadata from writings and projects pages

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,9 +1,5 @@
 import ProjectsList from "@/components/projects-list";
 
-export const metadata = {
-  title: "Projects",
-};
-
 export default function ProjectsPage() {
   return <ProjectsList />;
 }
--- src/app/writings/page.tsx
+++ src/app/writings/page.tsx
@@ -1,9 +1,5 @@
 import WritingsList from "@/components/writings-list";
 
-export const metadata = {
-  title: "Writings",
-};
-
 export default function WritingsPage() {
   return <WritingsList />;
 }
