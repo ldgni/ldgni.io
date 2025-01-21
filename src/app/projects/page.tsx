405ed63075712eedb928e81861aabd42cd0f74d5
Luca Di Gianni <luca.dgni@gmail.com>
2025-01-21 12:47:11 +0100

    fix: add metadata for Projects and Writings pages

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,5 +1,9 @@
 import ProjectsList from "@/components/projects-list";
 
+export const metadata = {
+  title: "Projects",
+};
+
 export default function ProjectsPage() {
   return <ProjectsList />;
 }
