e455da58d7ebd8f8e1e7e4c53e772521552b9dca
Luca Di Gianni <luca.dgni@gmail.com>
2025-02-25 11:06:19 +0100

    feat: add Clock component to footer for real-time display of date and time

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,10 @@
+import Clock from "@/components/ui/clock";
+
 export default function Footer() {
   return (
-    <footer className="mt-8">
+    <footer className="mt-8 flex justify-between">
       <small>&copy; 2025 Luca Di Gianni</small>
+      <Clock />
     </footer>
   );
 }
---
+++ src/components/ui/clock.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function Clock() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <small>
+      {time.toLocaleString("en-US", { month: "short", day: "numeric" })},{" "}
+      {time.toLocaleTimeString("en-US", { hour12: false })}
+    </small>
+  );
+}
