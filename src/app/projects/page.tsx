51e9e94e1a5ac4c23a1a68036fa083e88a526300
Luca Di Gianni <luca.dgni@gmail.com>
2025-05-07 18:28:18 +0200

    style: update border colors

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -29,7 +29,7 @@ export default function ProjectsPage() {
           <a
             href={project.url}
             target="_blank"
-            className="group block rounded-lg border border-neutral-400 px-4 py-2 saturate-0 transition hover:bg-neutral-200 hover:saturate-100 focus:bg-neutral-200 focus:saturate-100 focus:outline-none active:bg-neutral-200 active:saturate-100 dark:border-neutral-600 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800 dark:active:bg-neutral-800">
+            className="group block rounded-lg border border-neutral-300 px-4 py-2 saturate-0 transition hover:bg-neutral-200 hover:saturate-100 focus:bg-neutral-200 focus:saturate-100 focus:outline-none active:bg-neutral-200 active:saturate-100 dark:border-neutral-700 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800 dark:active:bg-neutral-800">
             <div className="flex items-center gap-4">
               <project.icon className="size-8 scale-95 text-neutral-500 transition group-hover:scale-100 group-hover:text-neutral-700 group-focus:scale-100 group-focus:text-neutral-700 group-active:scale-100 group-active:text-neutral-700 dark:group-hover:text-neutral-300 dark:group-focus:text-neutral-300 dark:group-active:text-neutral-300" />
               <div className="min-w-0">
