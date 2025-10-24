6b7ffe1e86e0e096b54c7708ea82675f3c892aba
Luca Di Gianni <luca.dgni@gmail.com>
2025-10-24 18:27:02 +0200

    refactor: simplify clock component

---
+++ components/clock.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function Clock() {
+  const [time, setTime] = useState<Date | null>(null);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  if (!time) {
+    return <time>...</time>;
+  }
+
+  const formattedDate = time.toLocaleString("en-US", {
+    month: "short",
+    day: "numeric",
+  });
+  const formattedTime = time.toLocaleTimeString("en-US", { hour12: false });
+
+  return (
+    <time dateTime={time.toISOString()}>
+      {formattedDate}, {formattedTime}
+    </time>
+  );
+}
