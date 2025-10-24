45da4bd037893da0ed650d073e90e79a0b21d416
Luca Di Gianni <luca.dgni@gmail.com>
2025-10-24 19:12:34 +0200

    feat: add ViewTransition

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { ViewTransition } from "react";
 
 import Footer from "@/components/footer";
 import Header from "@/components/header";
@@ -21,14 +22,16 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.className}>
       <body className="bg-neutral-900 text-neutral-100 antialiased">
-        <div className="pointer-events-none fixed size-full">
-          <div className="noise absolute inset-[-200%] bg-[url('/noise.webp')] opacity-[3%]" />
-        </div>
-        <div className="mx-auto max-w-lg p-4 sm:py-12 lg:py-20">
-          <Header />
-          {children}
-          <Footer />
-        </div>
+        <ViewTransition>
+          <div className="pointer-events-none fixed size-full">
+            <div className="noise absolute inset-[-200%] bg-[url('/noise.webp')] opacity-[3%]" />
+          </div>
+          <div className="mx-auto max-w-lg p-4 sm:py-12 lg:py-20">
+            <Header />
+            {children}
+            <Footer />
+          </div>
+        </ViewTransition>
       </body>
     </html>
   );
