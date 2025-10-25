296ad0eb9adc8731796044d6c85ecb930a787605
Luca Di Gianni <luca.dgni@gmail.com>
2025-10-25 11:54:26 +0200

    fix: remove ViewTransition

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import "./globals.css";
 
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { ViewTransition } from "react";
 
 import Footer from "@/components/footer";
 import Header from "@/components/header";
@@ -20,19 +19,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ViewTransition>
-      <html lang="en" className={inter.className}>
-        <body className="bg-neutral-900 text-neutral-100 antialiased">
-          <div className="pointer-events-none fixed size-full">
-            <div className="noise absolute inset-[-200%] bg-[url('/noise.webp')] opacity-[3%]" />
-          </div>
-          <div className="mx-auto max-w-lg p-4 sm:py-12 lg:py-20">
-            <Header />
-            {children}
-            <Footer />
-          </div>
-        </body>
-      </html>
-    </ViewTransition>
+    <html lang="en" className={inter.className}>
+      <body className="bg-neutral-900 text-neutral-100 antialiased">
+        <div className="pointer-events-none fixed size-full">
+          <div className="noise absolute inset-[-200%] bg-[url('/noise.webp')] opacity-[3%]" />
+        </div>
+        <div className="mx-auto max-w-lg p-4 sm:py-12 lg:py-20">
+          <Header />
+          {children}
+          <Footer />
+        </div>
+      </body>
+    </html>
   );
 }
