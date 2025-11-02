ca55a27aa58ed4f3a663eebea7b9d506d2a97930
Luca Di Gianni <luca.dgni@gmail.com>
2025-11-02 12:13:31 +0100

    fix: remove dateTime attribute from time element

--- components/time.tsx
+++ components/time.tsx
@@ -11,7 +11,7 @@ export default function Time() {
   }, []);
 
   return (
-    <time dateTime={time.toISOString()} suppressHydrationWarning>
+    <time suppressHydrationWarning>
       {time.toLocaleString("en-US", {
         month: "short",
         day: "numeric",
