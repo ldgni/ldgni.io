a2bc6d04d5b7db8967225e24a9c44bc56cf95125
Luca Di Gianni <luca.dgni@gmail.com>
2025-11-02 12:07:03 +0100

    refactor: simplify time component

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,11 +1,11 @@
-import Clock from "@/components/clock";
+import Time from "@/components/time";
 
 export default function Footer() {
   return (
     <footer className="mt-8 flex justify-between text-neutral-400">
       <small>&copy; 2025 Luca Di Gianni</small>
       <small>
-        <Clock />
+        <Time />
       </small>
     </footer>
   );
---
+++ components/time.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function Time() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => setTime(new Date()), 1000);
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <time>
+      {time.toLocaleString("en-US", {
+        month: "short",
+        day: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: false,
+      })}
+    </time>
+  );
+}
